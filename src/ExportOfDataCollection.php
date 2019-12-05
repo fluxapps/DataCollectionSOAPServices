f8effa9de45eaf58666931d522b463075ce42c78
jdolf <jd@studer-raimann.ch>
2019-12-05 15:26:55 +0100

    Add xlsx export support

--- src/ExportOfDataCollection.php
+++ src/ExportOfDataCollection.php
@@ -2,11 +2,21 @@
 
 namespace srag\Plugins\DataCollectionSOAPServices;
 
+use ilDclContentExporter;
+use ilDclException;
 use ilExport;
 use ilObject;
+use ilSoapPluginException;
 
 class ExportOfDataCollection extends Base
 {
+
+    const AVAILABLE_EXPORT_FORMATS
+        = [
+            "xlsx",
+            "xml",
+        ];
+
 
     /**
      * @inheritDoc
@@ -14,23 +24,39 @@ class ExportOfDataCollection extends Base
     protected function getAdditionalInputParams()
     {
         return array(
-            "ref_id" => Base::TYPE_INT,
+            "ref_id"        => Base::TYPE_INT,
+            "export_format" => Base::TYPE_STRING,
         );
     }
 
 
     /**
      * @inheritDoc
+     * @throws ilDclException
      */
     protected function run(array $params)
     {
-        // Possible errors: obj_id doesn't exist, various export errors
+        $requested_format = strtolower($params["export_format"]);
+
+        if (!in_array($requested_format, self::AVAILABLE_EXPORT_FORMATS)) {
+            //throw new ilSoapPluginException("Could not Read the XML File");
+        }
+
         $type = "dcl";
         $ref_id = $params["ref_id"];
         $obj_id = ilObject::_lookupObjectId($ref_id);
 
-        $exp = new ilExport();
-        $exp->exportObject($type, $obj_id);
+        if ($requested_format === "xlsx") {
+            $exporter = new ilDclContentExporter($ref_id);
+            $exporter->exportAsync();
+        } else if ($requested_format === "xml") {
+            $exp = new ilExport();
+            $exp->exportObject($type, $obj_id);
+        }
+
+        // Possible errors: obj_id doesn't exist, various export errors
+
+
     }
 
 
