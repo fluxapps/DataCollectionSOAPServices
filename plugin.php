b9eb01d29512f41284795a6b99fba0febc81df7e
fluxms <martin@fluxlabs.ch>
2022-02-03 02:52:41 +0100

    ilias7

--- plugin.php
+++ plugin.php
@@ -1,16 +1,9 @@
 <?php
-
 // alphanumerical ID of the plugin; never change this
 $id = "soapdc";
-
 // code version; must be changed for all code changes
-$version = "1.0.0";
-
-// ilias min and max version; must always reflect the versions that should
-// run with the plugin
-$ilias_min_version = "5.3.0";
-$ilias_max_version = "5.4.999";
-
-// optional, but useful: Add one or more responsible persons and a contact email
-$responsible = "studer + raimann ag - Team Core 1";
-$responsible_mail = "support-core1@studer-raimann.ch";
+$version = "1.1.0";
+$ilias_min_version = "6.0";
+$ilias_max_version = "7.999";
+$responsible = "fluxlabs ag";
+$responsible_mail = "support@fluxlabs.ch";
